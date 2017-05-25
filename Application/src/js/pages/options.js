73ee6398006b861fb4d47009d867b5e56362d6ee
brent-edwards-online <brent@brentedwardsonline.com>
2017-05-26 03:31:14 +1000

    Save latest files

--- Application/src/js/pages/options.js
+++ Application/src/js/pages/options.js
@@ -1,18 +1,31 @@
 import React from "react";
 
-import Article from "../components/Article";
+import Flight from "../components/flight";
 
 export default class Options extends React.Component {
   constructor() {
         super();
-        this.state = { items: [] };
+        this.state = { flights: [], carriers: [] };
     }
     
     componentDidMount() {
-        fetch(`http://localhost:8088/api/this/is/an/api/call`) 
-            .then(result=> {
-                this.setState({items:result.json()});
-            });
+        /*
+        fetch(`../../data/flight-options.json`) 
+          .then( (response) => {
+            return response.json()    
+          })
+          .then( (json)=> {
+            this.setState( {flights:json} );
+            console.log( 'Json Result',json );
+          });
+        */
+      
+        var flights = fetch(`../../data/flight-options.json`).then((response)=>response.json());
+        var carriers = fetch(`../../data/carriers.json`).then((response)=>response.json());
+        Promise.all([flights,carriers])
+          .then(requestData => {
+            this.setState( {flights:requestData[0], carriers: requestData[1]} );  
+        });
     }
     
   render() {
@@ -21,21 +34,12 @@ export default class Options extends React.Component {
     const { article } = params;
     const { date, filter } = query;
 
-    const Articles = [
-      "Some Article",
-      "Some Other Article",
-      "Yet Another Article",
-      "Still More",
-      "Fake Article",
-      "Partial Article",
-      "American Article",
-      "Mexican Article",
-    ].map((title, i) => <Article key={i} title={title}/> );
-
+    //const FlightOptions = this.state.flights.map((flight, i) => <Flight key={i} flight={flight, this.state.carriers.find(x => x.Id === flight.CarrierId)}/> );
+    const FlightOptions = this.state.flights.map((flight, i) => <Flight key={i} flight={flight} carrier={this.state.carriers.find(x => x.Id === flight.CarrierId)}/> );
     return (
       <div class="container">
         <h1>Flight Options</h1>
-        <div class="row">{Articles}</div>
+        <div class="row">{FlightOptions}</div>
       </div>
     );
   }
